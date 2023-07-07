ee6c83cc94ce58e325a1c53b82920d348eb435ef
Gabriella Ferruzzi <137997837+Gabry82@users.noreply.github.com>
2023-07-07 14:43:47 +0200

    Added a grid variable to optimization

--- yalmip_model.m
+++ yalmip_model.m
@@ -5,12 +5,16 @@ clear
 %tbxmanager restorepath
 %tbxmanager savepath
 yalmip('clear');
+% Loading data (loads)
+load dati.mat
+
 Ng=3; %Number of power plants
 Nt=3; %Number of thermic plants
 Nl=1; %Number of loads
 Nlt=1; %Number of thermic loads
 Nls=1; %Number of shiftable loads
 % cost function alpha and beta coefficients
+betagrid=prezzi_medi;
 alpha1= 0.0021/1000; beta1=34.4/1000;      % 
 alpha2=(0.042/1000); beta2= (25.78/1000);  % 
 alpha3= 0; beta3=1e-4;     % 
@@ -21,19 +25,17 @@ beta=[beta1;beta2;beta3];
 alphaT=diag([alpha1;alpha2;alpha3]);
 betaT=[beta1;beta2;beta3];
 
-
-
-% Loading data (loads)
 % L is an array (Nl,24)
-load dati.mat
 L=Potenze_attive_carico.';
 % Thermic load
-LT=Potenze_attive_carico.';
+LT=Potenze_termiche.';
 % LS is the array of shiftable loads (Nls,1)
 LS =200;
 % Constraints on co-generators power
 Gmax=repmat([10000;100;1000],1,24);
-Gmin=repmat([-10000;100;-1000],1,24);
+Gmin=repmat([0;100;-1000],1,24);
+Gridmax=repmat(10000,1,24);
+Gridmin=repmat(-10000,1,24);
 
 % Constraints on thermic power to electric grid
 THEmax=repmat([10000;100;1000],1,24);
@@ -53,6 +55,7 @@ ETHmax=1e8*[1;1;1]; %Maximum thermic capacity storage
 %Design variables
 act=binvar(Nls,24);
 G=sdpvar(Ng,24); %electric power
+Grid=sdpvar(1,24); %Grid electric power
 E=sdpvar(Ng,24); %capacity storage
 THE=sdpvar(Ng,24); %thermic power
 THT=sdpvar(Ng,24); %thermic power
@@ -84,19 +87,20 @@ for i=1:24
     end
     %Constraint on co-generator power (minimum and maximum)
     constraints=[constraints;G(:,i)<=Gmax(:,i);G(:,i)>=Gmin(:,i)];
+    constraints=[constraints;Grid(1,i)<=Gridmax(1,i);Grid(1,i)>=Gridmin(1,i)];
     constraints=[constraints;THT(:,i)<=THTmax(:,i);THT(:,i)>=THTmin(:,i)];
     constraints=[constraints;THE(:,i)<=THEmax(:,i);THE(:,i)>=THEmin(:,i)];
     %Equality constraint
-    constraints=[constraints;sum(G(:,i))+sum(etaE.*THE(:,i))==sum(L(:,i))+sum(act(:,i).*LS)];
+    constraints=[constraints;sum(G(:,i))+Grid(1,i)+sum(etaE.*THE(:,i))==sum(L(:,i))+sum(act(:,i).*LS)];
     constraints=[constraints;sum(etaT.*THT(:,i))==sum(LT(:,i))];
     %Objective function
-    J=J+G(:,i).'*alpha*G(:,i)+beta'*G(:,i)+...
+    J=J+G(:,i).'*alpha*G(:,i)+beta'*G(:,i)+betagrid(i)*Grid(1,i)+...
         (THT(:,i)+THE(:,i)).'*alphaT*(THT(:,i)+THE(:,i))+betaT'*(THT(:,i)+THE(:,i));
 end
 % options: currently the optimization is based on fmincon, to avoid cplex
 options = sdpsettings('solver','fmincon');
 % yalmip function to generate an object "optimizer" (prova is only a dummy variable. Do not delete it!).
-opt1=optimizer(constraints,J,options,[prova],{G,E,THT,THE,ETH,J,act});
+opt1=optimizer(constraints,J,options,[prova],{G,Grid,E,THT,THE,ETH,J,act});
 % Optimizer launch:
 % Results are in the variable "res", a cell array
 % The following function can be launched by an interpreted matlab function
@@ -109,9 +113,10 @@ opt1=optimizer(constraints,J,options,[prova],{G,E,THT,THE,ETH,J,act});
 % Jres is the objective function
 % act_res is the array to indicate the activation of shiftable loads
 Gres=res{1};
-Eres=res{2};
-THTres=res{3};
-THEres=res{4};
-ETHres=res{5};
-Jres=res{6};
-act_res=res{7};
+Gridres=res{2};
+Eres=res{3};
+THTres=res{4};
+THEres=res{5};
+ETHres=res{6};
+Jres=res{7};
+act_res=res{8};
